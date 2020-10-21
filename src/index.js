c05402ee2d3f87652fa090ef6552a53d9dbad805
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-10-22 01:14:48 +0300

    добавляет разметку карточек

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './styles.css';
+import cardTemplate from './templates/cardTemplate.hbs';
+import menuCards from './scripts/menuCards';
+
+
+
+
+
+
+const menuCardsList = menuCards.reduce((acc, menuCards) => {
+    return acc + cardTemplate(menuCards)
+}, '')
+
+
+
+
+// const menuCardList = [
+//     {
+//     id: "id",
+//     name: "name",
+//     description: "description",
+//     image: '',
+//     price: "price",
+//     ingredients: []
+// }]
+console.log(menuCardsList);
