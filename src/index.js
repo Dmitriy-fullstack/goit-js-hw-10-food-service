8663b83d518db3e3c8cd6dfce13a44a0daecbdf3
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-10-22 21:08:00 +0300

    1

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import cardTemplate from './templates/cardTemplate.hbs';
 import menuCards from './scripts/menuCards';
 
 
-
-
-
+const mainMenuEl = document.querySelector('.js-menu');
 
 const menuCardsList = menuCards.reduce((acc, menuCards) => {
     return acc + cardTemplate(menuCards)
 }, '')
 
-const mainMenuEl = document.querySelector('.js-menu');
 mainMenuEl.insertAdjacentHTML("beforeend", menuCardsList);
-
-
-
-
 console.log(menuCardsList);
+
+
