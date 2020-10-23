1c3ce6cc93b32cc2de4fe3a4c26568a1f2cde83c
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-10-23 20:20:49 +0300

    добавляет смену темы

--- src/index.js
+++ src/index.js
@@ -12,4 +12,23 @@ const menuCardsList = menuCards.reduce((acc, menuCards) => {
 mainMenuEl.insertAdjacentHTML("beforeend", menuCardsList);
 console.log(menuCardsList);
 
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
 
+const bodyEl = document.querySelector('body')
+const inputEl = document.querySelector('#theme-switch-toggle');
+inputEl.addEventListener('change', onInputClick);
+
+function onInputClick (evt) {
+    if (evt.target.checked) {
+        bodyEl.classList.add(Theme.DARK);
+        bodyEl.classList.remove(Theme.LIGHT);
+        localStorage.setItem('Theme', Theme.DARK);
+      } else {
+        bodyEl.classList.add(Theme.LIGHT);
+        bodyEl.classList.remove(Theme.DARK);
+        localStorage.setItem('Theme', Theme.LIGHT);
+      }
+}
